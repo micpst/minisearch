d5d2f580ddf1bb266a6e89aace338a7c92dcd0a3
micpst <m.pstrag.kontakt@gmail.com>
2023-03-02 23:50:49 +0100

    Fix tests

--- pkg/tokenizer/tokenizer_test.go
+++ pkg/tokenizer/tokenizer_test.go
@@ -12,22 +12,29 @@ type TestCase[Given any, Expected any] struct {
 	expected Expected
 }
 
+type TokenizeInput struct {
+	params TokenizeParams
+	config Config
+}
+
 type TokenizeOutput struct {
 	tokens []string
 	err    error
 }
 
 func TestTokenize(t *testing.T) {
-	tr := New(&Config{
-		EnableStemming:  true,
-		EnableStopWords: true,
-	})
 	cases := []TestCase[TokenizeInput, TokenizeOutput]{
 		{
 			given: TokenizeInput{
-				Text:            "",
-				Language:        ENGLISH,
-				AllowDuplicates: false,
+				params: TokenizeParams{
+					Text:            "",
+					Language:        ENGLISH,
+					AllowDuplicates: false,
+				},
+				config: Config{
+					EnableStemming:  true,
+					EnableStopWords: true,
+				},
 			},
 			expected: TokenizeOutput{
 				tokens: []string{},
@@ -36,31 +43,49 @@ func TestTokenize(t *testing.T) {
 		},
 		{
 			given: TokenizeInput{
-				Text:            "it's alive! it's alive!",
-				Language:        ENGLISH,
-				AllowDuplicates: false,
+				params: TokenizeParams{
+					Text:            "it's alive! it's alive!",
+					Language:        ENGLISH,
+					AllowDuplicates: false,
+				},
+				config: Config{
+					EnableStemming:  true,
+					EnableStopWords: true,
+				},
 			},
 			expected: TokenizeOutput{
-				tokens: []string{"it'", "aliv", "it'"},
+				tokens: []string{"it", "aliv"},
 				err:    nil,
 			},
 		},
 		{
 			given: TokenizeInput{
-				Text:            "it's alive! it's alive!",
-				Language:        ENGLISH,
-				AllowDuplicates: true,
+				params: TokenizeParams{
+					Text:            "it's alive! it's alive!",
+					Language:        ENGLISH,
+					AllowDuplicates: true,
+				},
+				config: Config{
+					EnableStemming:  true,
+					EnableStopWords: true,
+				},
 			},
 			expected: TokenizeOutput{
-				tokens: []string{"it'", "aliv", "it'", "aliv"},
+				tokens: []string{"it", "aliv", "it", "aliv"},
 				err:    nil,
 			},
 		},
 		{
 			given: TokenizeInput{
-				Text:            "Lorem ipsum. Dolor? Sit amet!",
-				Language:        "pl",
-				AllowDuplicates: false,
+				params: TokenizeParams{
+					Text:            "Lorem ipsum. Dolor? Sit amet!",
+					Language:        "pl",
+					AllowDuplicates: false,
+				},
+				config: Config{
+					EnableStemming:  true,
+					EnableStopWords: true,
+				},
 			},
 			expected: TokenizeOutput{
 				tokens: nil,
@@ -70,7 +95,7 @@ func TestTokenize(t *testing.T) {
 	}
 	for _, c := range cases {
 		t.Run(fmt.Sprintf("'%v'", c.given), func(t *testing.T) {
-			actual, err := tr.Tokenize(&c.given)
+			actual, err := Tokenize(&c.given.params, &c.given.config)
 
 			assert.Equal(t, c.expected.tokens, actual)
 			assert.Equal(t, c.expected.err, err)
